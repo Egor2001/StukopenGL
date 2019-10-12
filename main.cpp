0b8f31b549d70602936991cc9b18071ea9a85145
Egor Elchinov <elchinov.es@phystech.edu>
2019-10-12 20:34:08 +0300

    Camera class added

--- main.cpp
+++ main.cpp
@@ -19,24 +19,27 @@
 #include "src/CBuffer.h"
 #include "src/CObject.h"
 #include "src/CObject.h"
+#include "src/CCamera.h"
+#include "src/CCamera.h"
 
 int main()
 {
-    float ang = float(M_PI / 12.0f);
+    CCamera cam = CCamera(SVector( 1.0f,  1.0f,  1.0f),
+                          SVector(-1.0f, -1.0f, -1.0f));
 
-    SVector vec_x = SVector(cosf(ang), -sinf(ang), 0.0f, 0.0f);
-    SVector vec_y = SVector(sinf(ang),  cosf(ang), 0.0f, 0.0f);
-    SVector vec_z = SVector(0.0f,       0.0f,      1.0f, 0.0f);
+    SMatrix mtx = cam.get_matrix();
+    for (size_t y = 0; y < 4; ++y)
+        printf("%.3f %.3f %.3f %.3f \n", 
+                mtx[y][0], mtx[y][1], mtx[y][2], mtx[y][3]);
 
-    SMatrix mtx = sgl_new_basis_mtx(vec_x, vec_y, vec_z); 
-    mtx = mtx*mtx*mtx;
-
-    SVector vec = SVector(sqrtf(2.0f), sqrtf(2.0f), 2.0f);
-
+    float coord = 1.0f - sqrtf(3.0f)/3.0f;
+    SVector vec = SVector(coord, coord, coord);
     printf("initial vec: %.3f %.3f %.3f %.3f \n", 
             vec.x, vec.y, vec.z, vec.w);
+    
     vec = mtx*vec;
-    printf("rotated vec: %.3f %.3f %.3f %.3f \n", 
+
+    printf("camera vec: %.3f %.3f %.3f %.3f \n", 
             vec.x, vec.y, vec.z, vec.w);
 
     return 0;
--- src/SVector.h
+++ src/SVector.h
@@ -18,77 +18,97 @@ SVector operator + (const SVector& right, const SVector& left);
 SVector operator - (const SVector& right, const SVector& left);
 SVector operator * (const SVector& vector, float scale); 
     
-SVector unitary(const SVector& vector);
-
 SVector vector_mul(const SVector& right, const SVector& left);
 float   scalar_mul(const SVector& right, const SVector& left);
+
+float   abs    (const SVector& vector);
+SVector unitary(const SVector& vector);
+SVector normal (const SVector& vector);
 
 struct SVector
 {
 public:
-    SVector():
-        x(0.0f), y(0.0f), z(0.0f), w(1.0f)
-    {}
+    SVector();
+    SVector(float x_set, float y_set, float z_set, float w_set = 1.0f);
 
-    SVector(float x_set, float y_set, float z_set, float w_set = 1.0f):
-        x(x_set), y(y_set), z(z_set), w(w_set)
-    {}
+          float& operator [] (size_t idx)       noexcept;
+    const float& operator [] (size_t idx) const noexcept;
+       
+    SVector operator - () const;
 
-    float& operator [] (size_t idx) noexcept
-    {
-        switch (idx)
-        {
-            case 0: return x;
-            case 1: return y;
-            case 2: return z;
-            case 3: return w;
-            default: return w;                 
-        }
-    }
- 
-    const float& operator [] (size_t idx) const noexcept
-    {
-        switch (idx)
-        {
-            case 0: return x;
-            case 1: return y;
-            case 2: return z;
-            case 3: return w;
-            default: return w;                 
-        }
-    }
-   
-    SVector& operator += (const SVector& add)
-    {
-        x = x*add.w + add.x*w; 
-        y = y*add.w + add.y*w; 
-        z = z*add.w + add.z*w; 
-        w *= add.w;
+    SVector& operator += (const SVector& add);
+    SVector& operator -= (const SVector& sub);
+    SVector& operator *= (float scale);
 
-        return *this;
-    }
-    
-    SVector& operator -= (const SVector& sub)
-    {
-        x = x*sub.w - sub.x*w; 
-        y = y*sub.w - sub.y*w; 
-        z = z*sub.w - sub.z*w; 
-        w *= sub.w;
-
-        return *this;
-    }
-    
-    SVector& operator *= (float scale)
-    {
-        x *= scale; 
-        y *= scale; 
-        z *= scale; 
-
-        return *this;
-    }
-
+public:
     float x, y, z, w;
 };
+
+SVector::SVector():
+    x(0.0f), y(0.0f), z(0.0f), w(1.0f)
+{}
+
+SVector::SVector(float x_set, float y_set, float z_set, float w_set):
+    x(x_set), y(y_set), z(z_set), w(w_set)
+{}
+
+float& SVector::operator [] (size_t idx) noexcept
+{
+    switch (idx)
+    {
+        case 0: return x;
+        case 1: return y;
+        case 2: return z;
+        case 3: return w;
+        default: return w;                 
+    }
+}
+
+const float& SVector::operator [] (size_t idx) const noexcept
+{
+    switch (idx)
+    {
+        case 0: return x;
+        case 1: return y;
+        case 2: return z;
+        case 3: return w;
+        default: return w;                 
+    }
+}
+
+SVector SVector::operator - () const
+{
+    return SVector(-x, -y, -z, w);
+}
+
+SVector& SVector::operator += (const SVector& add)
+{
+    x = x*add.w + add.x*w; 
+    y = y*add.w + add.y*w; 
+    z = z*add.w + add.z*w; 
+    w *= add.w;
+
+    return *this;
+}
+
+SVector& SVector::operator -= (const SVector& sub)
+{
+    x = x*sub.w - sub.x*w; 
+    y = y*sub.w - sub.y*w; 
+    z = z*sub.w - sub.z*w; 
+    w *= sub.w;
+
+    return *this;
+}
+
+SVector& SVector::operator *= (float scale)
+{
+    x *= scale; 
+    y *= scale; 
+    z *= scale; 
+
+    return *this;
+}
 
 SVector operator + (const SVector& right, const SVector& left)
 {
@@ -108,18 +128,6 @@ SVector operator * (const SVector& vector, float scale)
     return (result *= scale);    
 }
     
-SVector unitary(const SVector& vector)
-{
-    SVector result;
-
-    if (fabs(vector.w) < FLT_EPSILON)
-        result = SVector(FLT_MAX, FLT_MAX, FLT_MAX, 1.0f);
-    else
-        result = vector*(1.0f/vector.w);
-
-    return result; 
-}
-
 SVector vector_mul(const SVector& right, const SVector& left)
 {
     SVector result = {};
@@ -141,9 +149,41 @@ float scalar_mul(const SVector& right, const SVector& left)
     result += right.z*left.z;
 
     if (fabs(right.w*left.w) > FLT_EPSILON)
-        result /= right.w*left.w;
+        result /= (right.w*left.w);
     else
         result = FLT_MAX;
+
+    return result;
+}
+
+float abs(const SVector& vector)
+{
+    return sqrtf(scalar_mul(vector, vector));
+}
+
+SVector unitary(const SVector& vector)
+{
+    SVector result;
+
+    if (fabs(vector.w) < FLT_EPSILON)
+        result = SVector(FLT_MAX, FLT_MAX, FLT_MAX, 1.0f);
+    else
+        result = vector*(1.0f/vector.w);
+
+    return result; 
+}
+
+SVector normal(const SVector& vector)
+{
+    SVector result; 
+
+    float length = abs(vector);
+    if (length < FLT_EPSILON)
+        result = SVector();
+    else
+        result = unitary(vector*(1.0f/length));
+
+    result.w = 0.0f;
 
     return result;
 }
